e14eeb4d556c4c2e95e4032d3f24bd8ea85aa6ab
Joweezip <joelurenacr@gmail.com>
2024-06-01 15:09:57 -0400

    New Changes

--- src/Components/TorontoCity.jsx
+++ src/Components/TorontoCity.jsx
@@ -1,25 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import '../Styles/TorontoCity.css'
 export default function Toronto() {
   return (
     <>
-       <div className="BlankBox">
-       <h1 className="Title">Toronto City</h1>
-       </div>
-     
-     <div className="SecBlankBox">
-     <p>
-        Welcome to Toronto! This vibrant city boasts diverse neighborhoods, from
-        the historic charm of the Distillery District to the eclectic energy of
-        Kensington Market. Dont miss iconic sights like the CN Tower and the
-        scenic Harbourfront. Explore multicultural cuisine in neighborhoods like
-        Chinatown and Little Italy. With its mix of culture, cuisine, and
-        landmarks, Toronto offers an unforgettable experience for every visitor.
-      </p>
-     </div>
+      <div className="title">
+        <h1>Toronto City</h1>
+      </div>
       
-      <img className= "Image1" src="src\assets\toront_img1.jpeg" />
       <img src="src\assets\Toronto_img2.jpeg" alt="" className="Image2" />
 
     </>
